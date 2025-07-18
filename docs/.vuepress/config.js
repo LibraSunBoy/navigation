e4e024153f0f573b78f853ac1edf1d21c7d69a46
JYD <13027619526@163.com>
2025-07-18 17:05:30 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@
 module.exports = {
     title: '我的技术文档', // 网站标题
     description: '使用 VuePress 搭建的专业文档', // 网站描述
+    base: '/navigation/', // 仓库名称作为基础路径
     themeConfig: {
         // 左侧导航配置
         sidebar: [
