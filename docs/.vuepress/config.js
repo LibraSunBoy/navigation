757ad38d8afb6d4724a5b2cee1689d79624564e8
JYD <13027619526@163.com>
2025-07-18 16:35:55 +0800

    test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+// docs/.vuepress/config.js
+module.exports = {
+    title: '我的技术文档', // 网站标题
+    description: '使用 VuePress 搭建的专业文档', // 网站描述
+    themeConfig: {
+        // 左侧导航配置
+        sidebar: [
+            {
+                title: '指南', // 导航组标题
+                collapsable: false, // 是否可折叠
+                children: [
+                    '/guide/installation', // 对应 docs/guide/installation.md
+                    '/guide/configuration'
+                ]
+            },
+            {
+                title: '高级功能',
+                children: [
+                    '/advanced/plugins',
+                    '/advanced/theming'
+                ]
+            }
+        ]
+    }
+}
